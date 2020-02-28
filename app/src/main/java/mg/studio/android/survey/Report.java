b98f2e24f9cb17412325b0d4902c805d8892a71e
axhxa <hynyhux@qq.com>
2020-02-28 13:34:04 +0800

    commit

--- app/src/main/java/mg/studio/android/survey/Report.java
+++ app/src/main/java/mg/studio/android/survey/Report.java
@@ -3,12 +3,18 @@ package mg.studio.android.survey;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class Report extends AppCompatActivity {
 
+    TextView t;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_report);
+        t=findViewById(R.id.text);
+        Bundle bundle=this.getIntent().getExtras();
+        String result=bundle.getString("");
+        t.setText(result);
     }
 }
